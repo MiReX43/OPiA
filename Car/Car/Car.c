2a3c7458c4aba96f462332ffdaff3f52e0f46aba
MiReX43 <150377512+MiReX43@users.noreply.github.com>
2024-09-14 21:21:43 +0300

    Add files via upload

---
+++ Car/Car/Car.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+puts("	                                             ..,;:ccco0NNNWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWKdc;.                                                                                          ");
+puts("                                            ..;coddxk0XNWMMMMMMMMMWWMMWNK0OOOOOOOOOOOOOOOOOOOOOO0KNWMMWK00XNNWWMWXOxl,.                                                                                 ");
+puts("                                    ...;lxkOOKXWMMMMMMMMMMMWNXXK0kxONWO:..........................:kKXKo,.'',cx0XNWMWXOxc..                                                                             ");
+puts("                              ..';lx0KXNWWMMMMMMMMMMMMWNKOkdc'.....:KWk.                           .'ck0d'     .':dOKNWWNKkl,.                                                                          ");
+puts("                         .';coOXNNWWMMMMMMMMMMMMMMWX0xo:'.         .xXO'                              .lO0o'       .'cdOXWWWXOl,.                                                                       ");
+puts("                     .,cokKNWWMMMMMMMMMMMMMMMMWNKkl;.               ,k0:.                               'o00o'         .,lkKNWWXOo,.                                                                    ");
+puts("                 .'cdOKWMMMMMMMMMMMMMMMMMMMWNKd:..                  .oXO;                                 ,xK0o'          .':dKNWWKx:..                                                                 ");
+puts("              .;okKNWMMMMMMMMMMMMMMMMMMMMWKx:.                       cKNd.                                 .:kX0o'            .;oONWN0xc;,''''.............                                             ");
+puts("           .;o0NWMMMMMMMMMMMMMMMMMMMMMMW0o'.                         .oK0c.                                  .l0XKo'             .cKWMWWNNNXXXXXXXXXXXXK0Oxdlc:;;;,'..                                  ");
+puts("         ,oOXWMMMMMMMMMMMMMMMMMMMMMMMMMXl.............................:0WXo'...................................cONWKo,...........;dKWMMMMMMMMMMMMMMMMMMMMMWWWWWWWWNXKkdolcc;'.                          ");
+puts("      .,dKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKOkkkkkkkkkkkkkkkkkkkkkkkkkkkk0NMWNKOkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkOKWMMWXOkkkkkkkkk0XWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNKOxdoc,.                    ");
+puts("    .,xXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNX0Okl;..              ");
+puts("   'dXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNX0dc'.          ");
+puts(" .;kXWWWWWWWWWWWWWWWWWWMMMMMMMMMMMMMMMMMMWWWWWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWWWWWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWX0d:.       ");
+puts(" ..;::::::::::::::::::cdXMMMMMMMMMWWXOdlcc:::::cclokKNWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWWNKxoccc:::::ccldOXNWMMMMMMMMMMWWWWWWWWWWWWWWNNXOl'     ");
+puts(".......................:KMMMMMMWXOo:'.     ....    .';lOXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWXkc;..   .....     .':dKNMMMMMMW0o:,,,,,,,,,,,,,,,;'.     ");
+puts("00000000000000000000000XWMMMMWXx;.   ..,cdkO00Oxl;'.   .;o0NMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0o,.  ..,cdk000Oxl;..    .cONMMMMXl.                        ");
+puts("MMMMMMMMMMMMMMMMMMMMMMMMMMMMXx;.   'lOXNWWMWWMMWWNXOo;.  .,dXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWKo'  .,lkKNNWWWWWWWWNXOo,.   .l0WMMNx,.                       ");
+puts("NWMMMMMMMMMMMMMMMMMMMMMMMMWKl.   'dKWWNKxollcclox0NWWXx;.  .:0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNO:.  ,dXWWNKxolccclox0NWWXx;    :ONMMWXOxddddddddddddddddddxdol");
+puts("o0NNNNNNNNNNNNWWMMMMMMMMMMKc.  .c0WWXkc'.        .;xXWWKl.   :OWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMW0,  .c0WWNOc..        .:xKWWKo.   ;OWMMMMMMMMMMMMMMMMMMMMWNNNNNN");
+puts(" ';;;;;;;;;;;:ckNMMMMMMMWXd.  .cKMW0c.             .:OWWXd.   :KMMMMMMMWNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNXk:. .lXWWKc.             .cONMXl.  .dNMMMMMMMMMMMMMMMMMMW0dc:::::");
+puts("              .cXMMMMMMMWx'   cKWMK:                 ;kNM0;   'OWMMMMWXxlc:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::c:.   ,kWWO:.                ,OMWKl. .c0WMMMMMMMMMMMMMMMMMNd.      ");
+puts("    .:cccccccld0WMMMMMMMNo.  .oNMNx'                  :KMXo.  .xXWMMMWO,                                                                  .dXMNd.                 .xNMWk.  ,xNMMMMMMMMMMMMMMMMMWXkoccc;.");
+puts("    .l0NWMWWMMMMMMMMMMMMNl   .oWWKo.                  ;KMNx,  .oKWMMMMNkooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooool'  'xNMWo.                 .oXWMO.  ,xNMMMMMMMMMMMMMMMMMMMMMMWNk'");
+puts("      .ckKNWMMMMMMMMMMMMNl   .oNMNk'                 .cXMXl.  .dXWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNc  .oKWWd.                 .xNMWk.  ;kNMMMMMMMMMMMMMMMMMMMMMWKo' ");
+puts("        .'cx0XWMMMMMMMMMNl    :0WMXc.               .c0WM0,   'kWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNl   'kWW0c.                ;0MW0c. .lKWMMMMMMMMMMMMMMMMMMWX0o'   ");
+puts("            .;ldddddddddo,    .:KMWKo'             .l0WWKo.   .cddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddo'   .cKWWXo'             .l0WMXl.   ;odddddddddddddddddddl;.     ");
+puts("                               .:ONWNOo;.       .'lONMW0c.                                                                                  .:OWMW0o,.       .,lOXWW0c.                                 ");
+puts("                                 .lONWWXOxxddddxOKWWNKd,.                                                                                     'oKNWWXOxdddddxkKWWWKo'                                   ");
+puts("                                   .ck0XNWMMMMMMWNKxl'.                                                                                         'cx0XWMMMMMMMWX0kl.                                     ");
+puts("                                     .'cONWMMMWWNk;.                                                                                              .'dXWWMMMWWKl'.                                       ");
+}
