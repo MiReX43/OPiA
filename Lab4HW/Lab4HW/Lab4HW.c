c1dd1bed7745e6b5a40f8b6d57995da6e59e388d
Denis <den.girevoi_43@mail.ru>
2024-09-30 22:53:31 +0300

    Lab4HW

--- Lab4HW/Lab4HW/Lab4HW.c
+++ Lab4HW/Lab4HW/Lab4HW.c
@@ -4,6 +4,13 @@
 #include <math.h>
 #include <locale.h>
 
+double result(int b, int c, double a, double x )
+{
+    double res;
+    res = a + (b - c) * x;
+    return res;
+}
+
 
 void main()
 {
@@ -19,7 +26,7 @@ void main()
     printf("¬ведите заработок на каждой из остальных газет: ");
     scanf("%lf", &x);
 
-    res = a + (b - c) * x;
+    res = result(b,c,a,x);
 
     printf("ќбщий заработок мальчика за продажу %d газет: %.2f руб.\n", b, res);
 
